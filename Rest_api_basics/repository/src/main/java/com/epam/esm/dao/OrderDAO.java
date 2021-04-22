db0165054ba963d39692eaa5370b5e30b167c7f7
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-04-22 16:45:33 +0300

    Add interface OrderDAO

---
+++ Rest_api_basics/repository/src/main/java/com/epam/esm/dao/OrderDAO.java
@@ -0,0 +1,31 @@
+package com.epam.esm.dao;
+
+import com.epam.esm.entity.Order;
+
+import java.util.List;
+
+/**
+ * Interface describes the interaction with the database
+ * for operating with objects of the Order type.
+ *
+ * @author Haurylchyk Aliaksei
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public interface OrderDAO extends EntityDAO<Order> {
+
+    /**
+     * Returns all Orders stored in the database.
+     *
+     * @return all Orders stored in the database.
+     */
+    List<Order> findAll();
+
+    /**
+     * Returns all orders for user with a specific id.
+     *
+     * @param id User id.
+     * @return list of Orders.
+     */
+    List<Order> findOrdersByUserId(Integer id);
+}
