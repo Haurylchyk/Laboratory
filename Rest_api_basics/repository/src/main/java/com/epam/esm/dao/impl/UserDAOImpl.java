9a92f50ab3f07740eed5a3f5fde1199b2854a278
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-04-22 13:52:46 +0300

    Add class UserDAOImpl

---
+++ Rest_api_basics/repository/src/main/java/com/epam/esm/dao/impl/UserDAOImpl.java
@@ -0,0 +1,96 @@
+package com.epam.esm.dao.impl;
+
+import com.epam.esm.constant.ParamNameConstant;
+import com.epam.esm.dao.UserDAO;
+import com.epam.esm.entity.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Class implements interface UserDAO. Describes the interaction with the database
+ * for operating with objects of the User type.
+ *
+ * @author Haurylchyk Aliaksei
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private static final String FIND_USER_BY_ID = "SELECT DISTINCT e FROM User e WHERE e.id = :id";
+    private static final String DELETE_USER = "DELETE FROM User e WHERE e.id = :id";
+    private static final String FIND_ALL_USERS = "SELECT DISTINCT e FROM User e";
+    private static final String FIND_USER_BY_LOGIN = "SELECT DISTINCT e FROM User e WHERE e.login = :login";
+
+    /**
+     * The index of the first item in the list.
+     */
+    private static final int FIRST_ELEMENT_INDEX = 0;
+
+    /**
+     * Adds new User to database.
+     *
+     * @param user User entity to create.
+     * @return User entity.
+     */
+    @Override
+    public User save(User user) {
+        em.persist(user);
+        return find(user.getId()).get();
+    }
+
+    /**
+     * Returns User with specific id.
+     *
+     * @param id User id.
+     * @return Optional of User entity stored in the database.
+     */
+    @Override
+    public Optional<User> find(Integer id) {
+        List<User> userList = em.createQuery(FIND_USER_BY_ID, User.class).
+                setParameter(ParamNameConstant.ID, id).getResultList();
+        return userList.isEmpty() ? Optional.empty() : Optional.of(userList.get(FIRST_ELEMENT_INDEX));
+    }
+
+    /**
+     * Deletes User with specific id from database.
+     *
+     * @param id User id.
+     */
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        em.createQuery(DELETE_USER).setParameter(ParamNameConstant.ID, id).
+                executeUpdate();
+    }
+
+    /**
+     * Returns all Users stored in the database.
+     *
+     * @return all Users stored in the database.
+     */
+    @Override
+    public List<User> findAll() {
+        return em.createQuery(FIND_ALL_USERS, User.class).getResultList();
+    }
+
+    /**
+     * Returns User with specific login.
+     *
+     * @param login User login.
+     * @return Optional of User entity stored in the database.
+     */
+    @Override
+    public Optional<User> findByLogin(String login) {
+        List<User> userList = em.createQuery(FIND_USER_BY_LOGIN, User.class).
+                setParameter(ParamNameConstant.LOGIN, login).getResultList();
+        return userList.isEmpty() ? Optional.empty() : Optional.of(userList.get(FIRST_ELEMENT_INDEX));
+    }
+
+}
