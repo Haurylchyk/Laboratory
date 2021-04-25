77a99dd4e3dd96435128c2d45540617cb2953952
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-04-25 22:51:44 +0300

    Add class PaginationConstant

---
+++ Rest_api_basics/repository/src/main/java/com/epam/esm/constant/PaginationConstant.java
@@ -0,0 +1,14 @@
+package com.epam.esm.constant;
+
+/**
+ * Class includes constants for pagination.
+ *
+ * @author Haurylchyk Aliaksei
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class PaginationConstant {
+
+    public static final int NUMBER_TAGS_ON_PAGE = 3;
+
+}
