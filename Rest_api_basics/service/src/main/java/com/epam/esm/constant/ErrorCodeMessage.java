15e3b271c2ff5d9677797e9be70301ef92e2637a
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-04-26 00:40:08 +0300

    Make some additions to the class ErrorCodeMessage

--- Rest_api_basics/service/src/main/java/com/epam/esm/constant/ErrorCodeMessage.java
+++ Rest_api_basics/service/src/main/java/com/epam/esm/constant/ErrorCodeMessage.java
@@ -10,7 +10,19 @@ package com.epam.esm.constant;
 public class ErrorCodeMessage {
     public static final String ERROR_CODE_TAG_INVALID_DATA = "0110";
     public static final String ERROR_CODE_TAG_EXISTS = "0120";
-    public static final String ERROR_CODE_TAG_NOT_FOUND = "4040101";
-    public static final String ERROR_CODE_GС_INVALID_DATA = "0210";
-    public static final String ERROR_CODE_GС_NOT_FOUND = "4040200";
+    public static final String ERROR_CODE_TAG_NOT_FOUND = "4040100";
+
+    public static final String ERROR_CODE_GC_INVALID_DATA = "0210";
+    public static final String ERROR_CODE_GC_NOT_FOUND = "4040200";
+
+    public static final String ERROR_CODE_USER_INVALID_DATA = "0310";
+    public static final String ERROR_CODE_USER_EXISTS = "0320";
+    public static final String ERROR_CODE_USER_NOT_FOUND = "4040300";
+
+    public static final String ERROR_CODE_ORDER_INVALID_DATA = "0410";
+    public static final String ERROR_CODE_ORDER_NOT_FOUND = "4040400";
+
+
+
+
 }
