50c030bfd3c03cd4d8ade6f776e7ed20f1d50e7b
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-05-28 18:28:18 +0300

    Makes changes to the class UserController

--- Rest_api_basics/controller/src/main/java/com/epam/esm/UserController.java
+++ Rest_api_basics/controller/src/main/java/com/epam/esm/UserController.java
@@ -1,15 +1,23 @@
 package com.epam.esm;
 
 import com.epam.esm.assembler.UserModelAssembler;
-import com.epam.esm.dto.UserDTO;
+import com.epam.esm.model.CreatingUserData;
+import com.epam.esm.model.dto.AuthRequestDTO;
+import com.epam.esm.model.dto.UserDTO;
+import com.epam.esm.security.provider.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import java.util.List;
 
@@ -19,11 +27,25 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final UserModelAssembler userModelAssembler;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserController(UserService userService, UserModelAssembler userModelAssembler) {
+    public UserController(UserService userService, UserModelAssembler userModelAssembler, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
         this.userModelAssembler = userModelAssembler;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO signUp(@Valid @RequestBody CreatingUserData creatingUserData) {
+        return userModelAssembler.toModel(userService.signUp(creatingUserData));
+    }
+
+    @PostMapping("/auth")
+    public String login(@RequestBody AuthRequestDTO authRequestDTO) {
+        UserDTO user = userService.login(authRequestDTO);
+        return jwtTokenProvider.createToken(user.getLogin());
     }
 
     @GetMapping("/{id}")
