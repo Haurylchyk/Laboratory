5130609dc6702e0c2ccdfd8dbbd7aa8d5792b3d6
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-08-20 23:34:54 +0300

    Change CertificateDetailsComponent

--- certificate-ui/src/app/features/certificates/components/certificate-details/certificate-details.component.ts
+++ certificate-ui/src/app/features/certificates/components/certificate-details/certificate-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GiftCertificate} from '../../../../core/model/gift-certificate';
 import {GiftCertificateService} from '../../../../core/service/gift-certificate.service';
 import {AuthenticationService} from '../../../../core/service/authentication.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Statistic} from '../../../../core/model/statistic';
+import {CartManagerService} from '../../../cart/service/cart-manager.service';
 
 @Component({
   selector: 'app-certificate-details',
@@ -12,25 +14,41 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class CertificateDetailsComponent implements OnInit {
   certificate: GiftCertificate;
   certificateId: number;
+  statistic: Statistic;
+  isAdminRole: boolean;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private certificateService: GiftCertificateService,
     public authService: AuthenticationService,
+    private cartManager: CartManagerService
   ) {
   }
 
   ngOnInit(): void {
+    this.isAdminRole = this.authService.hasRole('ADMIN');
     this.certificateId = this.activatedRoute.snapshot.params.id;
     this.certificateService.getCertificateById(this.certificateId)
       .subscribe(
         (certificate) => {
           this.certificate = certificate;
+        }, error => {
+          this.router.navigate(['/404']);
+        }
+      );
+    this.certificateService.getStatistic()
+      .subscribe(
+        (statistic) => {
+          this.statistic = statistic;
         });
   }
 
-  redirectToEditCertificatePage(certificateId: number): void {
-    this.router.navigateByUrl(`certificates/${certificateId}/edit`);
+  addCertificateToCart(certificate: GiftCertificate): void {
+    this.cartManager.addCertificate(certificate);
+  }
+
+  redirectToCertificatesPage(): void {
+    this.router.navigate(['/certificates']);
   }
 }
