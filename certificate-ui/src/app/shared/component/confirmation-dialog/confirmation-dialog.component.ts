4996926698024cc98621f2638709b8014592cddd
Aliaksei Haurylchyk <eydolss@gmail.com>
2021-08-20 23:07:45 +0300

    Add ConfirmationDialogComponent

---
+++ certificate-ui/src/app/shared/component/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,31 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {
+  }
+
+  dialogTitle: string;
+
+  ngOnInit(): void {
+    console.log('Confirmation dialog was opened!');
+    this.dialogTitle = this.data;
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+}
